81fcf95378926bc6b1d7b29ce30c0d56c4de9260
Admin <ahmadqawsmie2020@gmail.com>
2025-01-29 20:12:34 +0300

    32

---
+++ src/main/java/com/example/work/WorkApplication.java
@@ -0,0 +1,16 @@
+package com.example.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkApplication.class, args);
+
+	}
+
+}
+//GOOGLE_CLOUD_PROJECT=anamil-e8d76
+//GOOGLE_APPLICATION_CREDENTIALS=C:\Users\Admin\Downloads\anamil-firebase-adminKey.json
